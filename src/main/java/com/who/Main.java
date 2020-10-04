fd0833c552fc7f41877acfc3cc257db6016bc68d
Eazzzy3 <blafubla@mail.ru>
2020-10-04 19:59:30 +0300

    версия которая вроде запускется

--- src/main/java/com/who/Main.java
+++ src/main/java/com/who/Main.java
@@ -3,6 +3,22 @@ package com.who;
 public class Main {
     public static void main(String[] args) {
         System.out.println("!!!!!!!!!!!!");
-        String key = args[0];
+        String txt = args[0];
+        String key = args[1];
+
+        byte[] txtByte = txt.getBytes();
+        byte[] keyByte = key.getBytes();
+
+        for (int i =0; i<txt.length(); i++) {
+            txtByte[i] ^= keyByte[i%key.length()];
+            System.out.println(new String(txtByte));
+        }
+        System.out.println("__________");
+
+        for (int i =0; i<txt.length(); i++) {
+            txtByte[i] ^= keyByte[i%key.length()];
+            System.out.println(new String(txtByte));
+        }
+
     }
 }
