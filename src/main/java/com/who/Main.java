60010ee9bb06121d47703c7908b1305ec5dd9df3
Eazzzy3 <blafubla@mail.ru>
2020-10-06 21:27:42 +0300

    не работает когда key > 127 (отрицательный в byte)

--- src/main/java/com/who/Main.java
+++ src/main/java/com/who/Main.java
@@ -4,124 +4,100 @@ package com.who;
 import java.io.File;
 import java.io.IOException;
 import java.io.PrintWriter;
-import java.io.BufferedReader;
-import java.io.File;
-import java.io.FileReader;
-import java.io.IOException;
-import java.nio.charset.StandardCharsets;
 import java.nio.file.Files;
 import java.nio.file.Paths;
-import java.util.Scanner;
+import java.util.Arrays;
 
 public class Main {
 
-    public static void main(String[] args) {
 
+    public static void main(String[] args) { //     0     1      2     3      4
+//                                                -c/-d  key  in.txt  -o   out.name
         try {
 
-            File inName = new File(args[0]);  //вход
-            System.out.println("in:    " + inName);
-            String keyString = args[1];
-            System.out.println("key:   " + keyString);
-            File outName = new File(args[2]); //выход
-            System.out.println("out:   " + outName);
-            System.out.println("");
+            boolean format = true;
 
 
-            if (!outName.exists()) //если выход не сделан то делаю
-                outName.createNewFile();
+            //легальность
+            if (args.length != 3 && args.length != 5) {   //3 или 5 аргументов
+                format = false;
+            } else if (!args[0].equals("-c") && !args[0].equals("-d")) { //первый аргумент -d/-с (шифрация / дешифрация)
+                format = false;
+            } else if (args[1].length() % 2 == 1) {    //не целое количество байт в ключе
+                format = false;
+            } else if (args.length == 5 && !args[3].equals("-o")) {      //четвертый аргумент должен быть -o (после него ожидается путь к out файлу)
+                format = false;
+            }//легальность end
+            // !!!дописать regex для key!!!
 
-            Scanner inScan = new Scanner(inName);
-            String inTxt = "";
 
-            while (inScan.hasNext()) { //весь текст в строчку
-                inTxt += inScan.nextLine();
-                if (inScan.hasNext()) inTxt += "\r\n";
-            }
-
-            System.out.println(inTxt);
+            String inName = args[2];  //имя входного файла
 
 
-            PrintWriter outTxt = new PrintWriter(outName); //тут буду писать
+            //определение имени out файла
+            File outName;
+            if (args.length == 5 && args[3].equals("-o")) {
+                outName = new File(args[4]);
+            } else {
+                outName = new File(inName + "Out");
+            }//определение имени out файла enf
 
-            byte[] txtByte = inTxt.getBytes();
-            byte[] keyByte = keyString.getBytes();
 
-            for (int i = 0; i < txtByte.length; i++) {
-                txtByte[i] ^= keyByte[i % keyByte.length];
+            if (format) {
+                if (args[0].equals("-c")) System.out.println("Шифрация файла " + inName + " начата.");
+                if (args[0].equals("-d")) System.out.println("Дешифрация файла " + inName + " начата.");
+
+                if (!outName.exists()) //делаю файл out если такого нет
+                    outName.createNewFile();
+
+
+                PrintWriter outTxt = new PrintWriter(outName); //тут буду писать
+
+                byte[] txtByte = Files.readAllBytes(Paths.get(inName));   //в байт. виде текст
+
+                //создание ключа
+                String keyString = args[1];
+                byte[] keyByte = new byte[keyString.length() / 2]; //в байт. виде ключ
+                for (int i = 0; i < keyString.length() / 2; i++){
+                    keyByte[i] = (byte) Integer.parseInt(keyString.substring(i * 2, i * 2 + 2), 16);
+                }//создание ключа end
+
+
+
+                System.out.println("Key:  " + Arrays.toString(keyByte));
+                System.out.println();
                 System.out.println(new String(txtByte));
-                System.out.println("___________");
+
+                for (int i = 0; i < txtByte.length; i++) {  //старт кодирования
+
+                    txtByte[i] ^= keyByte[i % keyByte.length];
+
+
+
+                }
+
+
+                outTxt.println(new String(txtByte));
+                outTxt.close();
+                System.out.println("Новый файл находится в " + outName);
+
             }
-            outTxt.println(new String(txtByte));
-            outTxt.close();
-
-
-            System.out.println("");
-            System.out.println("______________________________________________________");
-            System.out.println("");
-
 
         } catch (IOException e) {
             System.out.println("Error: " + e);
         }
 
-        if (args.length>3) {
-            try {
-
-                File inName = new File(args[2]);  //вход
-                System.out.println("in:    " + inName);
-                String keyString = args[1];
-                System.out.println("key:   " + keyString);
-                File outName = new File(args[3]); //выход
-                System.out.println("out:   " + outName);
-                System.out.println("");
 
 
-                if (!outName.exists()) //если выход не сделан то делаю
-                    outName.createNewFile();
 
-                Scanner inScan = new Scanner(inName);
-                String inTxt = "";
-
-                while (inScan.hasNext()) { //весь текст в строчку
-                    inTxt += inScan.nextLine();
-                    if (inScan.hasNext()) inTxt += "\r\n";
-                }
-
-                System.out.println(inTxt);
-
-
-                PrintWriter outTxt = new PrintWriter(outName); //тут буду писать
-
-                byte[] txtByte = inTxt.getBytes();
-                byte[] keyByte = keyString.getBytes();
-
-                for (int i = 0; i < txtByte.length; i++) {
-                    txtByte[i] ^= keyByte[i % keyByte.length];
-                    System.out.println(new String(txtByte));
-                    System.out.println("___________");
-                }
-                outTxt.println(new String(txtByte));
-                outTxt.close();
-
-
-                System.out.println("");
-                System.out.println("______________________________________________________");
-                System.out.println("");
-
-
-            } catch (IOException e) {
-                System.out.println("Error: " + e);
-            }
-        }
 
 
         System.out.println();
         System.out.println();
         System.out.println();
-        System.out.println("!!!!!!!!!!!!");
-
         System.out.println("end");
+
+
     }
 }
 
